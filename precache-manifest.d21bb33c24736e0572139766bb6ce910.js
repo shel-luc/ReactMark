63e5c21ca08d01260e02225ef2d5fe32d1e011f8
shel-luc <51696132+shel-luc@users.noreply.github.com>
2020-03-23 16:33:23 -0400

    Updates

---
+++ precache-manifest.d21bb33c24736e0572139766bb6ce910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c633b33f2f136278d584bf1222e6867",
+    "url": "/ReactMark/index.html"
+  },
+  {
+    "revision": "472f2c12b6b1d73afd88",
+    "url": "/ReactMark/static/css/main.b2128c5f.chunk.css"
+  },
+  {
+    "revision": "15845dafc4220bfcb2e0",
+    "url": "/ReactMark/static/js/2.d8aff80a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactMark/static/js/2.d8aff80a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "472f2c12b6b1d73afd88",
+    "url": "/ReactMark/static/js/main.d45007f9.chunk.js"
+  },
+  {
+    "revision": "3fece1e653a2fceb6a75",
+    "url": "/ReactMark/static/js/runtime-main.509cea3a.js"
+  }
+]);
